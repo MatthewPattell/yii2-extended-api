ad7bbe62a1915be8d7dccbe758eb8e649b92ddfd
Mikhail Yarmoluk <hellboymxb@gmail.com>
2018-10-30 13:12:16 +0300

    Change check access create action

--- src/ECreateAction.php
+++ src/ECreateAction.php
@@ -7,7 +7,11 @@
 
 namespace MP\ExtendedApi;
 
+use Yii;
 use yii\rest\CreateAction;
+use yii\base\Model;
+use yii\helpers\Url;
+use yii\web\ServerErrorHttpException;
 
 /**
  * Class    ECreateAction
@@ -17,5 +21,33 @@ use yii\rest\CreateAction;
  */
 class ECreateAction extends CreateAction
 {
+    /**
+     * Creates a new model.
+     * @return \yii\db\ActiveRecordInterface the model newly created
+     * @throws ServerErrorHttpException if there is any error when creating the model
+     */
+    public function run()
+    {
+        /* @var $model \yii\db\ActiveRecord */
+        $model = new $this->modelClass([
+            'scenario' => $this->scenario,
+        ]);
 
+        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
+
+        if ($this->checkAccess) {
+            call_user_func($this->checkAccess, $this->id, $model);
+        }
+
+        if ($model->save()) {
+            $response = Yii::$app->getResponse();
+            $response->setStatusCode(201);
+            $id = implode(',', array_values($model->getPrimaryKey(true)));
+            $response->getHeaders()->set('Location', Url::toRoute([$this->viewAction, 'id' => $id], true));
+        } elseif (!$model->hasErrors()) {
+            throw new ServerErrorHttpException('Failed to create the object for unknown reason.');
+        }
+
+        return $model;
+    }
 }
